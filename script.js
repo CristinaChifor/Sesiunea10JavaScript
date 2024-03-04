760a3860e9b2eda53b94e3388ee80572b5da45b5
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-03-04 19:14:22 +0200

    Addition examples

--- script.js
+++ script.js
@@ -12,8 +12,25 @@ console.log (username);
 
 const piNumber = 3.14;
 console.log (piNumber);
-piNumber = 4;
+// piNumber = 4;
 //val setate in constante nu pot fi schimbate
+
+// Operatori pentru date numerice
+// Operatori artimetici
+
+// % operator care calc operatii cu rest
+
+// Addition +
+
+let  firstNumber =12;
+let secondNumber = 21;
+let sum = firstNumber + secondNumber;
+console.log (`Rezultatul sumei dintre ${firstNumber} si ${secondNumber} este ${sum}`);
+//string interpolation ${}
+// trecem let o singura data cand def prima data variabila.
+// nu trebuie trecut decat o singura data.
+
+
 
 
 
