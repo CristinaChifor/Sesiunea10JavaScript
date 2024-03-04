db236015dafc78e2c566018794bbc02e5f5433ac
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-03-04 19:06:23 +0200

    add const

--- script.js
+++ script.js
@@ -10,5 +10,10 @@ let username = "Cristina";
 console.log (" name:");
 console.log (username);
 
+const piNumber = 3.14;
+console.log (piNumber);
+piNumber = 4;
+//val setate in constante nu pot fi schimbate
+
 
 
