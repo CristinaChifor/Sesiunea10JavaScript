a2ffd618505eb7a27eb5abadc46adf34204d0110
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-03-05 14:20:13 +0200

    Add string data type

---
+++ script2.js
@@ -0,0 +1,19 @@
+let age = 30;
+console.log(age);
+console.log(`I am ${age} years old.`);
+
+let years = 5;
+console.log(years);
+
+// let defineste o variabila in cazul de fata age;
+// mai jos nu am mai folosit let deoarece am reatribuit o noua valoare variabilei age
+age= 36;
+console.log(age);
+
+let username = "Cristina";
+console.log(`My name is ${username}`);
+console.log('username: ');
+console.log(usernamename);
+
+// string-ul este o valoare absoluta.
+
