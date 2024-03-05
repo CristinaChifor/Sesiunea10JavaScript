c3c8725999cec58580aca6de7441ca2767d532a7
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-03-05 15:15:27 +0200

    Add addition examples

--- script2.js
+++ script2.js
@@ -23,4 +23,20 @@ console.log(userName);
 // string-ul este o valoare absoluta.
 
 // Tipuri de date numerice: Number
-// Operatori date nmerice
+// Operatori date numerice: ARITMETICI
+
+// Addition +
+
+let firstNumber = 10;
+firstNumber=12;
+let secondNumber = 21;
+let sum = firstNumber + secondNumber;
+console.log(sum);
+console.log(`Rezultatul sumei dintre ${firstNumber} si ${secondNumber} este ${sum}.`);
+
+// cand reatribuim val de dinainte este stearsa, nu se mai ia in considerare
+// ii este luat locul de catre noua valoare
+
+// string interpolation se foloseste pentru a crea texte mai interpretative
+
+
